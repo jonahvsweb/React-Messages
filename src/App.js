fe47d88f0e33007f3e6a64d82cbd3908837c2f6e
Jonah Bitautas <jbitautas@gmail.com>
2022-07-06 00:50:01 -0500

    Add message view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import './App.css';
+import MessageView from './message-view';
+
 class App extends Component {
 
     render() {
         return (
-            <div>
-                Suuup
-            </div>
+            <MessageView/>
         )
     }
 }
