4045c4f333e3101e99da45af234814f03fef7cc3
Jonah Bitautas <jbitautas@gmail.com>
2022-07-08 19:47:38 -0500

    Add multiple message views with map.

--- src/message-list.js
+++ src/message-list.js
@@ -4,17 +4,35 @@ import MessageView from "./message-view";
 
 class MessageList extends Component {
     state = {
-        message: {
-            from: 'Martha', 
-            content: 'I will be traveling soon.', 
-            status: 'read'
-        }
+        messages: [
+            {
+                _id: `d2504a54`, 
+                from: 'John', 
+                content: 'The event starts next week.', 
+                status: 'unread'
+            }, 
+            {
+                _id: `fc7cad74`, 
+                from: 'Martha', 
+                content: 'I will be traveling soon.', 
+                status: 'read'
+            }, 
+            {
+                _id: `876ae642`, 
+                from: 'Jacob', 
+                content: 'Talk later. Have a great day!', 
+                status: 'read'
+            }
+        ]
     }
     render() {
+        const messageViews = this.state.messages.map(
+            message => <MessageView key={message._id} message={message} />
+        )
         return(
             <div className="container">
                 <h1>Your Messages</h1>
-                <MessageView message={this.state.message} />
+                {messageViews}
             </div>
         )
     }
